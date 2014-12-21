f2bd68d68fb6b0fcd68b04fb997fb6ee31d9372e
HrNilsson <renenilsson01@gmail.com>
2014-12-21 14:35:52 +0100

    Add java ttest class

---
+++ TestSerial/SyncReportMsg.java
@@ -0,0 +1,12 @@
+
+
+public class SyncReportMsg{
+	private unsigned short node id;
+	private unsigned int timeEst;
+	private unsigned byte syncPeriod;
+	private float drift;
+	private unsigned short temp;
+	private unsigned byte seqNo;
+
+
+}
